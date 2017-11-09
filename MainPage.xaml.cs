13024df7862e511038b030c2c92cb00b36190979
Angel D. Munoz <scythes.lol@gmail.com>
2017-11-09 16:23:25 -0600

    add sample code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,19 +12,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using QuinCalc.Models;
+using System.Collections.ObjectModel;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace QuinCalc
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public ObservableCollection<Todo> Todos = new ObservableCollection<Todo>(){
+      new Todo() { Id = 1, Name = "Some Todo 1", IsDone = true },
+      new Todo() { Id = 2, Name = "Some Todo 2", IsDone = false },
+      new Todo() { Id = 3, Name = "Some Todo 3", IsDone = true },
+      new Todo() { Id = 4, Name = "Some Todo 4", IsDone = false },
+      new Todo() { Id = 5, Name = "Some Todo 5", IsDone = true },
+      new Todo() { Id = 6, Name = "Some Todo 6", IsDone = false },
+    };
+
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
     }
+  }
 }
