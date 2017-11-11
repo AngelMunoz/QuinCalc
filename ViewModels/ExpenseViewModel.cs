6d7a5b24b8968623c8f8873f8b8f7d1b70b19cc4
Angel D. Munoz <scythes.lol@gmail.com>
2017-11-11 00:11:00 -0600

    Add Todo/Expense Viewmodels

---
+++ ViewModels/ExpenseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuinCalc.Models;
+
+namespace QuinCalc.ViewModels
+{
+  public class ExpenseViewModel : NotificationBase<Expense>
+  {
+    public ExpenseViewModel(Expense expense = null) : base(expense) { }
+
+    public string Name
+    {
+      get { return This.Name; }
+      set { SetProperty(This.Name, value, () => This.Name = value); }
+    }
+
+    public decimal Amount
+    {
+      get { return This.Amount; }
+      set { SetProperty(This.Amount, value, () => This.Amount = value); }
+    }
+
+    public DateTime DueDate
+    {
+      get { return This.DueDate; }
+      set { SetProperty(This.DueDate, value, () => This.DueDate = value); }
+    }
+  }
+}
---
+++ ViewModels/TodoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuinCalc.Models;
+
+namespace QuinCalc.ViewModels
+{
+  public class TodoViewModel : NotificationBase<Todo>
+  {
+    public TodoViewModel(Todo todo = null) : base(todo) { }
+
+    public string Name
+    {
+      get { return This.Name; }
+      set { SetProperty(This.Name, value, () => This.Name = value); }
+    }
+
+    public bool IsDone
+    {
+      get { return This.IsDone; }
+      set { SetProperty(This.IsDone, value, () => This.IsDone = value); }
+    }
+
+    public string Description
+    {
+      get { return This.Description; }
+      set { SetProperty(This.Description, value, () => This.Description = value); }
+    }
+
+    public DateTime DueDate
+    {
+      get { return This.DueDate; }
+      set { SetProperty(This.DueDate, value, () => This.DueDate = value); }
+    }
+  }
+}
