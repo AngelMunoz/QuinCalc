9721314960bbd34a155929c46c039246952c0043
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2018-11-09 21:10:16 -0600

    Add  Sample ValueConverter

--- Converters/DateTimeConverter.cs
+++ Converters/DateTimeConverter.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 
 namespace QuinCalc.Converters
 {
-  public class DateTimeConverter: IValueConverter
+  public class DateTimeConverter : IValueConverter
   {
     public object Convert(object value, Type targetType, object parameter, string language)
     {
-      return value;
+      DateTime.TryParse((value ?? "").ToString(), out DateTime date);
+      return date.ToShortDateString();
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, string language)
     {
-      throw new NotImplementedException();
+      DateTime.TryParse((value ?? "").ToString(), out DateTime date);
+      return date;
     }
   }
 }
