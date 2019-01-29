352a272fe86b9ce2513f437824ffa43d61ecc92a
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2019-01-29 15:23:12 -0700

    refactor(converters): convert to currency converter instead of just decimal converter

---
+++ Converters/CurrencyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace QuinCalc.Converters
+{
+  public class CurrencyConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+      string currency = ((decimal)value).ToString("C", CultureInfo.CurrentCulture);
+      return currency;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      var culture = new CultureInfo(language);
+      var preval = ((string)value).ToString(CultureInfo.CurrentCulture);
+      if (decimal.TryParse(preval, out decimal result)) { return result; }
+      return value;
+    }
+
+  }
+}
