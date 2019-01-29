d7e5c7e4e3a7b5eb32b7e9e3388bc114c59edd74
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2019-01-29 15:24:59 -0700

    feature(helpers): add NodeFinder for future usage refactor(helpers): move notificationbase into helpers

---
+++ Helpers/NodeFinder.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace QuinCalc.Helpers
+{
+  public static class NodeFinder
+  {
+    public static DependencyObject FindChildByName(DependencyObject from, string name)
+    {
+      int count = VisualTreeHelper.GetChildrenCount(from);
+
+      for (int i = 0; i < count; i++)
+      {
+        var child = VisualTreeHelper.GetChild(from, i);
+        if (child is FrameworkElement && ((FrameworkElement)child).Name == name)
+          return child;
+
+        var result = FindChildByName(child, name);
+        if (result != null)
+          return result;
+      }
+
+      return null;
+    }
+  }
+}
