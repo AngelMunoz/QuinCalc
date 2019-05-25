d135c671b2d5102dd089f7170492dc0cc1ba37ee
Angel D. Munoz <scythes.lol@gmail.com>
2019-05-24 23:16:30 -0600

    tests(services): TodoService Test updated

--- QuinCalc.Core.Test/Services.Test/TodoService.Test.cs
+++ QuinCalc.Core.Test/Services.Test/TodoService.Test.cs
@@ -12,51 +12,108 @@ namespace QuinCalc.Core.Test.Services.Test
   [TestClass]
   public class TodoServiceTest
   {
-    private string dir;
-    private string dbstring;
+    private readonly TestHelper _helper = TestHelper.GetInstance();
 
-    [TestInitialize]
-    public async Task Startup()
+    [TestMethod]
+    [Priority(1)]
+    public async Task Create_Todo_Async_Test()
     {
-      dir = Path.GetTempPath();
-      dbstring = $"Data Source={dir}\\TodoServiceTest.db";
-      using (var context = new QuincalcContext(dbstring))
+      var todo10 = new Todo { Id = 11, Name = "Todo10", Description = "Created10", IsDone = true, DueDate = DateTimeOffset.UtcNow.AddDays(1) };
+      using (var todos = new TodoService(dbstring: _helper.DbString))
       {
-        await context.Database.MigrateAsync();
+        var r1 = await todos.CreateAsync(todo10);
+        Assert.IsTrue(r1);
       }
-    }
-
-    [TestCleanup]
-    public void Cleanup()
-    {
-      File.Delete($"{dir}\\TodoServiceTest.db");
+      using (var context = new QuincalcContext(_helper.DbString))
+      {
+        var exists = context.Todos.Any(t => t.Id == 11);
+        Assert.IsTrue(exists);
+      }
     }
 
     [TestMethod]
-    public async Task Create_Todo_Test()
+    [Priority(1)]
+    public async Task Update_Todo_Async_Test()
     {
-      int count;
-      int expetctedCount = 1;
-      Todo todo;
-      Todo expectedTodo = new Todo { Name = "Test Todo", Description = "This is a Test Todo", IsDone = false, DueDate = DateTimeOffset.Now.AddDays(1) };
-      using (var service = new TodoService(dbstring: dbstring))
+      var todo9 = new Todo { Id = 10, Name = "TodoUpdated9", Description = "Updated9", IsDone = true, DueDate = DateTimeOffset.UtcNow.AddDays(15) };
+      using (var todos = new TodoService(dbstring: _helper.DbString))
       {
-        await service.CreateAsync(expectedTodo);
-        var result = service.Find(0, 10);
-        count = result.Item1;
-        todo = result.Item2.FirstOrDefault();
+        var r1 = await todos.UpdateAsync(todo9);
+        Assert.IsTrue(r1);
       }
-      using (var context = new QuincalcContext(dbstring))
+      using (var context = new QuincalcContext(_helper.DbString))
       {
-        count = context.Todos.Count();
-        expectedTodo = context.Todos.FirstOrDefault();
+        var todo = context.Todos.First(t => t.Id == 10);
+        Assert.AreEqual("Updated9", todo.Description);
+        Assert.AreEqual("TodoUpdated9", todo.Name);
       }
-      Assert.AreEqual(expetctedCount, count);
-      Assert.IsInstanceOfType(todo, typeof(Todo));
-      Assert.AreEqual(expectedTodo.Name, todo.Name);
-      Assert.AreEqual(expectedTodo.Description, todo.Description);
-      Assert.AreEqual(expectedTodo.IsDone, todo.IsDone);
-      Assert.AreEqual(expectedTodo.DueDate, todo.DueDate);
+    }
+
+    [TestMethod]
+    [Priority(1)]
+    public async Task Destroy_Todo_Async_Test()
+    {
+      var todo6 = new Todo { Id = 7, Name = "Todo6", Description = "Description6", IsDone = false, DueDate = DateTimeOffset.UtcNow.AddDays(1) };
+      using (var todos = new TodoService(dbstring: _helper.DbString))
+      {
+        var r1 = await todos.DestroyAsync(todo6);
+        Assert.IsTrue(r1);
+      }
+      using (var context = new QuincalcContext(_helper.DbString))
+      {
+        var exists = context.Todos.Any(t => t.Id == 7);
+        Assert.IsFalse(exists);
+      }
+    }
+
+    [TestMethod]
+    [Priority(2)]
+    public async Task Find_Closest_Async_Todo__Test()
+    {
+      using (var todos = new TodoService(dbstring: _helper.DbString))
+      {
+        var todo = await todos.FindClosestAsync();
+        Assert.AreEqual(1, todo.Id);
+      }
+    }
+
+    [TestMethod]
+    [Priority(2)]
+    public void Find_Todo_Test()
+    {
+      using (var todos = new TodoService(dbstring: _helper.DbString))
+      {
+        var (count, unordered) = todos.Find(0, 3);
+        var list = unordered.OrderBy(t => t.Id);
+        Assert.AreEqual(10, count);
+        Assert.AreEqual(1, list.ElementAt(0).Id);
+        Assert.AreEqual(2, list.ElementAt(1).Id);
+        Assert.AreEqual(3, list.ElementAt(2).Id);
+      }
+    }
+
+    [TestMethod]
+    [Priority(2)]
+    public void Find_By_IsDone_Todo__Test()
+    {
+      using (var todos = new TodoService(dbstring: _helper.DbString))
+      {
+        var (count1, list1) = todos.FindByIsDone(true, 0, 5);
+        Assert.AreEqual(4, count1);
+        var ordered1 = list1.OrderBy(t => t.Id);
+        Assert.AreEqual(8, ordered1.ElementAt(0).Id);
+        Assert.AreEqual(9, ordered1.ElementAt(1).Id);
+        Assert.AreEqual(10, ordered1.ElementAt(2).Id);
+
+        var (count2, list2) = todos.FindByIsDone(false, 0, 5);
+        Assert.AreEqual(6, count2);
+        var ordered2 = list2.OrderBy(t => t.Id);
+        Assert.AreEqual(1, ordered2.ElementAt(0).Id);
+        Assert.AreEqual(2, ordered2.ElementAt(1).Id);
+        Assert.AreEqual(3, ordered2.ElementAt(2).Id);
+        Assert.AreEqual(4, ordered2.ElementAt(3).Id);
+        Assert.AreEqual(5, ordered2.ElementAt(4).Id);
+      }
     }
   }
 }
